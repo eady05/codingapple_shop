3a8c0d31e5c2704005d1685d4e38f54ef11f6a03
daye <dahee834@gmail.com>
2022-08-26 16:39:05 +0900

    shop_first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import bgimag from './img/bg.jpeg';
+import data from './data.js';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">CoCo Market</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg' style={{backgroundImage : 'url(https://mobiinsidecontent.s3.ap-northeast-2.amazonaws.com/kr/wp-content/uploads/2021/07/02094730/나이키1.png)'}}>
+      </div>
+      <div className='container'>
+        <Row>
+          <Col sm>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
+          </Col>
+          <Col sm>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"/>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content}</p>
+          </Col>
+          <Col sm>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"/>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].content}</p>
+          </Col>
+        </Row>
+    </div>
     </div>
   );
 }
